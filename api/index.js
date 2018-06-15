4dd6a05573a53960db9c7f311d45a7489d8160a9
manuel-lara <laranavm@oregonstate.edu>
2018-06-15 11:19:02 -0700

    small changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const router = module.exports = require('express').Router();
 
 router.use('/restaurants', require('./restaurants').router);
-// router.use('/reviews', require('./reviews').router);
+router.use('/reviews', require('./reviews').router);
 router.use('/hotels', require('./hotels').router);
 router.use('/users', require('./users').router);
