5fa525e74b36b757284067791e0faf428d735040
Austin Sanders <sanderau@oregonstate.edu>
2018-06-15 12:36:11 -0700

    Filling in the hotels table

--- db-init/00-db-init.sql
+++ db-init/00-db-init.sql
@@ -40,6 +40,15 @@ CREATE TABLE `Hotels` (
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+LOCK TABLES `Hotels` WRITE;
+
+INSERT INTO `Hotels` VALUES (NULL, "Marriot", "1234 Cherry Street", "Corvallis", "OR", "95762", "1231231234", "fake.com", "fake@fake.global", "123123123123", 4.0, 3.5);
+INSERT INTO `Hotels` VALUES (NULL, "Zeus' Beard", "1234 Olympic Court", "Corvallis", "OR", "97330", "1231899234", "fake.com", "fake@fake.com", "12301223123", 3.0, 4.5);
+INSERT INTO `Hotels` VALUES (NULL, "Odin's Raven", "1234 Valhalla Drive", "Corvallis", "OR", "97330", "1276834", "fake.com", "notReal@fake.global", "1231238623123", 3.0, 1.5);
+INSERT INTO `Hotels` VALUES (NULL, "Beds, Beds, Beds", "1234 Repitition Street", "Corvallis", "OR", "97330", "129154234", "fake.com", "fake@fake.global", "00123123", 5.0, 2.5);
+INSERT INTO `Hotels` VALUES (NULL, "WaterFall Beds", "1234 Dry Street", "Corvallis", "OR", "97330", "12300134", "fake.com", "fake@fake.global", "18912123", 2.0, 4.5);
+
+UNLOCK TABLES;
 DROP TABLE IF EXISTS `Restaurants`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
